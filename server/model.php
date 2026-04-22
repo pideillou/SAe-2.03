6174af76a7a4af58d6559c3e03e4589bb215680d
Pideill Paul <pideill2@mmi-dev.unilim.fr>
2026-04-22 14:18:32 +0200

    Test

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "pideill2");
+define("DBLOGIN", "pideill2");
+define("DBPWD", "pideill2");
 
 
 function getAllMovies(){
